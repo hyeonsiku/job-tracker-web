1eb4a484e77935aa89e2ee08c7b35a154470351f
hyeonsiku <atio0@naver.com>
2026-09-23 10:49:11 +0900

    fix: include runtime config in Vite build

---
+++ public/js/config.js
@@ -0,0 +1,8 @@
+window.SUPABASE_CONFIG = {
+  url: "https://kkvinqltevqxzerkmbro.supabase.co",
+  publishableKey: "sb_publishable_ztRI4Jps77g2i7ENtk8gxQ_yb5PUF1S"
+};
+
+window.GOOGLE_CONFIG = {
+  clientId: "851211851030-n0rh8c55k2cncef0danj67nsh7mnskl9.apps.googleusercontent.com"
+};
